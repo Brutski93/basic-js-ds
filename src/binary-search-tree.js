4100ba291e1cbca7f97f049cd67c8b1d3e3595f7
Brutski93 <abrutski@gmail.com>
2025-10-26 17:37:46 +0300

    feat: almost solve binary-search-tree

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -1,47 +1,141 @@
 const { NotImplementedError } = require('../lib/errors');
-// const { Node } = require('../extensions/list-tree.js');
+const { Node } = require('../extensions/list-tree.js');
 
 /**
 * Implement simple binary search tree according to task description
 * using Node from extensions
 */
 class BinarySearchTree {
+
+  constructor() {
+    this.start = null;
+  }
+
   root() {
-    // Remove line below and write your code here
-    throw new NotImplementedError('Not implemented');
+    return this.start;
   }
 
-  add(/* data */) {
-    // Remove line below and write your code here
-    throw new NotImplementedError('Not implemented');
+  add(data) {
+    const newNode = new Node(data);
+
+    if (!this.start) {
+      this.start = newNode;
+      return;
+    }
+    
+    let currentNode = this.start;
+    while (currentNode) {
+      if (newNode.data === currentNode.data) return;
+      if (newNode.data < currentNode.data) {
+        if (!currentNode.left) {
+          currentNode.left = newNode;
+          return;
+        }
+        currentNode = currentNode.left;
+      } else {
+        if (!currentNode.right) {
+          currentNode.right = newNode;
+          return;
+        }
+        currentNode = currentNode.right;
+      }
+    }
   }
 
-  find(/* data */) {
-    // Remove line below and write your code here
-    throw new NotImplementedError('Not implemented');
+  find(data) {
+    let currentNode = this.start;
+    while (currentNode) {
+      if (currentNode.data === data) return currentNode;
+      if (data < currentNode.data) currentNode = currentNode.left;
+      else currentNode = currentNode.right;
+    }
+    return null;
   }
 
-  has(/* data */) {
-    // Remove line below and write your code here
-    throw new NotImplementedError('Not implemented');
+  has(data) {
+    return this.find(data) ? true : false;
   }
 
-  remove(/* data */) {
-    // Remove line below and write your code here
-    throw new NotImplementedError('Not implemented');
+  remove(data) {
+    if (!this.find(data)) return;
+    if (this.start.data === data) {
+      this.start = null;
+      return;
+    }
+
+    function deleteNode2(obj, data) {
+      if (obj.data > data) {
+        if (obj.left.data === data) {
+          obj.left = null;
+          return;
+        }
+        deleteNode2(obj.left, data);
+      }
+      else {
+        if (obj.right.data === data) {
+          obj.right = null;
+          return;
+        }
+        deleteNode2(obj.right, data);
+      }
+    }
+
+    deleteNode2(this.start, data);
   }
 
-  min() {
-    // Remove line below and write your code here
-    throw new NotImplementedError('Not implemented');
+  min(start = this.start) {
+    if (!this.start) return null;
+    let currentNode = start;
+    let min = currentNode.data;
+    while (currentNode.left) {
+      min = currentNode.left.data;
+      currentNode = currentNode.left;
+    }
+    return min;
   }
 
   max() {
-    // Remove line below and write your code here
-    throw new NotImplementedError('Not implemented');
+    if (!this.start) return null;
+    let currentNode = this.start;
+    let max = currentNode.data;
+    while (currentNode.right) {
+      max = currentNode.right.data;
+      currentNode = currentNode.right;
+    }
+    return max;
+  }
+  visual(obj = this.start) {
+    if (!this.start) return;
+    let left = null;
+    let right = null;
+    if (obj.left) left = obj.left.data;
+    if (obj.right) right = obj.right.data;
+    console.log(`Node: ${obj.data} left: ${left} right: ${right}`);
+    if (obj.left) this.visual(obj.left);
+    if (obj.right) this.visual(obj.right);
   }
 }
 
 module.exports = {
   BinarySearchTree
 };
+
+const tree = new BinarySearchTree();
+      tree.add(9);
+      tree.add(14);
+      tree.add(2);
+      tree.add(6);
+      tree.add(128);
+      tree.add(8);
+      tree.add(31);
+      tree.add(54);
+      tree.add(1);
+      tree.remove(14);
+      tree.visual()
+      tree.remove(8);
+      tree.visual()
+      tree.remove(9);
+      tree.visual()
+console.log(
+  tree
+);
