608dd631ae31cecbb5c9e35ba07c745fec8a7972
Brutski93 <abrutski@gmail.com>
2025-10-30 19:44:36 +0300

    feat: almost solve remove-from-list

--- src/remove-from-list.js
+++ src/remove-from-list.js
@@ -1,5 +1,5 @@
 const { NotImplementedError } = require('../lib/errors');
-// const { ListNode } = require('../extensions/list-node.js');
+const { ListNode } = require('../extensions/list-node.js');
 
 /**
  * Given a singly linked list of integers l and an integer k,
@@ -21,11 +21,61 @@ const { NotImplementedError } = require('../lib/errors');
  *   }
  * }
  */
-function removeKFromList(/* l, k */) {
-  // Remove line below and write your code here
-  throw new NotImplementedError('Not implemented');
+function removeKFromList(l, k) {
+  let currentList = l;
+  let parentList = null;
+  while (currentList) {
+    if (currentList.value === k) {
+      if (currentList.next) {
+        currentList.value = currentList.next.value;
+        currentList.next = currentList.next.next;
+      } else {
+        parentList.next = null;
+        return;
+      }
+    } else {
+      parentList = currentList;
+      currentList = currentList.next;
+    }
+  }
+  return l;
 }
 
 module.exports = {
   removeKFromList
 };
+
+const arr = [1, 2, 3];
+const list = convertArrayToList(arr);
+
+function convertArrayToList(arr, index = 0) {
+  if (index === arr.length) return null;
+  const list = new ListNode(arr[index]);
+  list.next = convertArrayToList(arr, index + 1);
+  return list;
+}
+
+function convertListToArray(list) {
+  if (!list.value) return [];
+  const arr = [list.value];
+  let curreentList = list;
+  while (curreentList.next) {
+    arr.push(curreentList.next.value);
+    curreentList = curreentList.next;
+  }
+  return arr;
+}
+
+function consoleList(obj) {
+  let currentObj = obj;
+  while (currentObj) {
+    console.log(`value: ${currentObj.value} next: ${currentObj.next}`);
+    currentObj = currentObj.next;
+  }
+}
+
+consoleList(list);
+console.log(convertListToArray(list))
+removeKFromList(list, 3);
+consoleList(list);
+console.log(convertListToArray(list))
