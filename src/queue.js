e98eb8f107aa4273e3bc235cf7f68ed8ec5cfa89
Brutski93 <abrutski@gmail.com>
2025-10-30 19:06:36 +0300

    feat: solve queue

--- src/queue.js
+++ src/queue.js
@@ -1,5 +1,5 @@
 const { NotImplementedError } = require('../lib/errors');
-// const { ListNode } = require('../extensions/list-node.js');
+const { ListNode } = require('../extensions/list-node.js');
 
 /**
  * Implement the Queue with a given interface via linked list (use ListNode extension above).
@@ -12,23 +12,55 @@ const { NotImplementedError } = require('../lib/errors');
  * queue.dequeue(); // returns the top element from queue and deletes it, returns 1
  * queue.getUnderlyingList() // returns { value: 3, next: null }
  */
+// class Queue {
+//   getUnderlyingList() {
+//     // Remove line below and write your code here
+//     throw new NotImplementedError('Not implemented');
+//   }
+
+//   enqueue(/* value */) {
+//     // Remove line below and write your code here
+//     throw new NotImplementedError('Not implemented');
+//   }
+
+//   dequeue() {
+//     // Remove line below and write your code here
+//     throw new NotImplementedError('Not implemented');
+//   }
+// }
 class Queue {
-  getUnderlyingList() {
-    // Remove line below and write your code here
-    throw new NotImplementedError('Not implemented');
+  constructor() {
+    this.value = null;
+    this.next = null;
   }
-
-  enqueue(/* value */) {
-    // Remove line below and write your code here
-    throw new NotImplementedError('Not implemented');
+  enqueue(data, obj = this) {
+    if (!obj.value) {
+      obj.value = data;
+      return;
+    }
+    // if (!obj.next) obj.next = new ListNode(data);
+    if (!obj.next) obj.next = {value: data, next: null};
+    else this.enqueue(data, obj.next);
   }
-
   dequeue() {
-    // Remove line below and write your code here
-    throw new NotImplementedError('Not implemented');
+    if(!this.value) return undefined;
+    const temp = this.value;
+    this.value = this.next.value;
+    this.next = this.next.next;
+    return temp;
+  }
+  getUnderlyingList() {
+    if(!this.value) return null;
+    return {value: this.value, next: this.next};
+  }
+  test() {
+    let curr = this;
+    while (curr) {
+      console.log(curr.value, curr.next);
+      curr = curr.next
+    }
   }
 }
-
 module.exports = {
   Queue
 };
